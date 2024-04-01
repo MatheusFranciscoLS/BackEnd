654a57ea68355b4e7d07be03f20316a07ea95da4
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-04-01 20:50:40 -0300

    28/03/24

--- crud_escola/src/main/java/webapp/crud_escola/Repository/AdministradorRepository.java
+++ crud_escola/src/main/java/webapp/crud_escola/Repository/AdministradorRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import webapp.crud_escola.Model.Administrador;
 
 public interface AdministradorRepository extends CrudRepository<Administrador, String> {
+    Administrador findByCpf(String cpf);
+    Administrador findBySenha(String senha);
 
 }
