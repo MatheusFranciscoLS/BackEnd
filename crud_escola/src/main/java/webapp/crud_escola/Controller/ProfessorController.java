726d38d0ff2fa14c3a99ee746e5076733c8fec84
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-05-08 19:37:52 -0300

    08/05/24

--- crud_escola/src/main/java/webapp/crud_escola/Controller/ProfessorController.java
+++ crud_escola/src/main/java/webapp/crud_escola/Controller/ProfessorController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import ch.qos.logback.core.model.Model;
 import webapp.crud_escola.Model.Professor;
-import webapp.crud_escola.Model.Usuario;
 import webapp.crud_escola.Repository.ProfessoresRepository;
 import webapp.crud_escola.Repository.VerificaCadastroProfRepository;
 
